6e53aafe47f35032aabb052ea085441d53025d87
rakshika <rakshikaraju@gmail.com>
2022-11-27 19:32:13 -0500

    crud api for donorprofile after testing

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const express = require('express')
+import express from 'express';
+import cors from 'cors';
+//const express = require('express')
+import DonorController from "./controllers/donors/donor-controller.js";
 const app = express()
-app.get('/', (req, res) => {res.send('Web squad project')})
+app.use(cors())
+app.use(express.json());
+
+DonorController(app)
+// app.get('/', (req, res) => {res.send('Web squad project')})
 app.listen(4000)
+// console.log("hello!");
