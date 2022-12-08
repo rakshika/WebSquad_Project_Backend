549665e7d7efdfc36b49070283d4b26073b4f272
rakshika <rakshikaraju@gmail.com>
2022-12-08 01:52:03 -0500

    integrated with mongodb

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-//const express = require('express')
+import mongoose from 'mongoose';
+mongoose.connect('mongodb://127.0.0.1:27017/HungerSaviors');
 import DonorController from "./controllers/donors/donor-controller.js";
 const app = express()
 app.use(cors())
